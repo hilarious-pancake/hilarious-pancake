b834a9e5ead310710a702684c4c69e18f8d01578
Stephanie A. Higa <sahiga@gmail.com>
2015-03-12 14:50:49 -0700

    Draft of SQL schema

---
+++ db/config.js
@@ -0,0 +1,22 @@
+var sequelize = new Sequelize('sifter', 'root', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+var Item = sequelize.define('item', {
+  id: Sequelize.UUIDV1,
+  category: Sequelize.STRING,
+  description: Sequelize.STRING,
+  url: Sequelize.STRING,
+  date: Sequelize.DATE,
+  timestamps: true
+});
+
+
+sequelize.sync();
